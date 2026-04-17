68f82ed81a693efd8c32487722e376f87e9813b0
Mohand BEDJOU <mouhandbedjou687@gmail.com>
2026-04-17 19:17:44 +0100

    bouton resolve

--- favoris.php
+++ favoris.php
@@ -553,7 +553,7 @@ $totalFavoris = mysqli_num_rows($result);
       </div>
 
       <div id="user-dropdown" class="dropdown" style="display:none">
-        <a href="profil.php" class="dropdown-item">Mon profil</a>
+        <a href="monprofil.php" class="dropdown-item">Mon profil</a>
         <a href="mes_annonces.php" class="dropdown-item">Mes annonces</a>
         <a href="favoris.php" class="dropdown-item">Mes favoris</a>
         <hr style="border:none;border-top:0.5px solid var(--bd);margin:4px 0">
